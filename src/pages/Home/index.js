6a0631f03175e4d4b5c220d5a3fae7fd6c355a9f
LomeshGupta <guptabittu06@gmail.com>
2024-02-22 20:51:46 +0530

    prlegal

---
+++ src/pages/Home/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Home1 from "../../assets/home1.png";
+import Home2 from "../../assets/home1mobile.png";
+import "./home.css";
+
+const Home = () => {
+  return (
+    <section class="section1">
+      <div>
+        <div class="imgdiv">
+          <img src={Home1} width="100%" alt="..." />
+        </div>
+        <div class="imgdiv1">
+          <img src={Home2} width="100%" alt="..." />
+        </div>
+        <div className="heading">
+          Solving your <br></br>problems head-on
+        </div>
+        <div className="heading2">
+          We provide you with direct and expert legal care<br></br>so that you
+          can resolve issues early and amicably.
+        </div>
+        <div className="btn">
+          <a className="servicebtn" href="#">
+            see services
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
---
+++ src/pages/Navbar/index.js
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoClose } from "react-icons/io5";
+// import { ReactComponent as Brand } from '../../assets/icons/logo.svg'
+import "./nav.css";
+
+const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="logo">
+          {/* <Brand /> */}
+          PR Legal Help
+        </div>
+        <div className="menu-icon" onClick={handleShowNavbar}>
+          <GiHamburgerMenu />
+        </div>
+        <div className={`nav-elements  ${showNavbar && "active"}`}>
+          <ul className="nav__list">
+            <li className="nav__item">
+              <div className="menu-icon" onClick={handleShowNavbar}>
+                <IoClose />
+              </div>
+            </li>
+            <li className="nav__item">
+              <NavLink to="/" className="nav__link">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav__item">
+              <NavLink to="/news" className="nav__link">
+                News
+              </NavLink>
+            </li>
+            <li className="nav__item">
+              <NavLink to="/about-us" className="nav__link">
+                About Us
+              </NavLink>
+            </li>
+            <li className="nav__item">
+              <NavLink to="/favorite" className="nav__link">
+                Favorite
+              </NavLink>
+            </li>
+            <li className="nav__item">
+              <NavLink to="/location" className="nav__link">
+                Location
+              </NavLink>
+            </li>
+            <li className="nav__item">
+              <NavLink to="/get-started" className="nav__link nav__cta">
+                Contact Us
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
