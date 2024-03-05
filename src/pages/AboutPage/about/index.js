e892cfef380655c8133af5e3f0536aeb90e73434
LomeshGupta <guptabittu06@gmail.com>
2024-03-05 17:11:28 +0530

    section

--- src/pages/AboutPage/about/index.js
+++ src/pages/AboutPage/about/index.js
@@ -12,10 +12,17 @@ const About = () => {
         </div>
 
         <div className="center-content">
-          <h2>Centered Text</h2>
+          <h2>Who we are?</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel
-            nisi eu mauris tincidunt dignissim.
+            With a rich legacy spanning over a decade, PR Legal Help stands as a
+            stalwart in providing unparalleled legal solutions. Our commitment
+            to excellence is evident in our extensive track record, boasting
+            over 10 years of experience in delivering legal services with utmost
+            precision. At PR Legal Help, we understand that each client comes
+            with unique needs and concerns. Therefore, our approach is rooted in
+            the principle of personalized attention and unwavering support. We
+            prioritize building strong client relationships, ensuring that every
+            individual receives the guidance and legal assistance they deserve.
           </p>
         </div>
 
