8c4d0471701cca87d6b840d1f05c8b4949230351
Lomesh Gupta <guptabittu06@gmail.com>
2024-06-01 00:27:07 +0530

    new

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,15 +1,12 @@
 import React, { Suspense } from "react";
+import { Helmet } from "react-helmet";
+import img from "../../assets/prlegal.png";
 
-// const NavBar = React.lazy(() => import("../../Navbar"));
+// Import your components
 const Home = React.lazy(() => import("../../Components/Home"));
 const BoxSection = React.lazy(() => import("../../Components/Secondsection"));
-const TestimonialSection = React.lazy(() =>
-  import("../../Components/testimonals")
-);
-// const BlogSlideshow = React.lazy(() => import("../../Components/Blogs"));
-const SoftwareExpertiseSection = React.lazy(() =>
-  import("../../Components/SoftwareExpertise")
-);
+const TestimonialSection = React.lazy(() => import("../../Components/testimonals"));
+const SoftwareExpertiseSection = React.lazy(() => import("../../Components/SoftwareExpertise"));
 const Home2 = React.lazy(() => import("../../Components/ourservice"));
 const Footer = React.lazy(() => import("../../footer"));
 const Whyus = React.lazy(() => import("../../Components/Whyus"));
@@ -17,6 +14,16 @@ const Whyus = React.lazy(() => import("../../Components/Whyus"));
 function App() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>PR LEGAL HELP</title>
+        <meta name="description" content="Best LEGAL Advisor Around PR LEGAL HELP is a dedicated supplier of legal service to legal professionals & Firms in Ireland, UK, Dubai, USA headquartered in Delhi, India, with a focus on delivering exceptional outsourcing services to our clients." />
+        <meta property="og:title" content="PR LEGAL HELP" />
+        <meta property="og:description" content="Best LEGAL Advisor Around PR LEGAL HELP is a dedicated supplier of legal service to legal professionals & Firms in Ireland, UK, Dubai, USA headquartered in Delhi, India, with a focus on delivering exceptional outsourcing services to our clients." />
+        <meta property="og:image" content={img} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+      </Helmet>
       <Home />
       <BoxSection />
       <Whyus />
